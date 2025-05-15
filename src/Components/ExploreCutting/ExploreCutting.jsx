03e9c3773d7f9d9c0e56a0119309f76e21786fc8
ayeshamukta <ayeshamukta0000@gmail.com>
2025-05-16 00:32:15 +0600

    updated the product images and designed the exploring gadgets section

--- src/Components/ExploreCutting/ExploreCutting.jsx
+++ src/Components/ExploreCutting/ExploreCutting.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
+import Product from '../Product/Product';
 
 
 const ExploreCutting = () => {
 
     const loadedData = useLoaderData();
-    console.log(loadedData);
+    // console.log(loadedData);
     
     return (
         <div>
             <h1>Explore Cutting-Edge Gadgets</h1>
-            <div>
-                <div></div>
-                <div></div>
+            <div className='grid grid-cols-4 gap-5'>
+                <div className='shadow-2xl flex flex-col p-8 h-min gap-5  rounded-2xl'>
+                    <button className="btn rounded-full">All Products</button>
+                    <button className="btn rounded-full">Laptops</button>
+                    <button className="btn rounded-full">Accessories</button>
+                    <button className="btn rounded-full">Smart Watches</button>
+                    <button className="btn rounded-full">MacBook</button>
+                    <button className="btn rounded-full">Iphone</button>
+                </div>
+                <div className='col-span-3 grid gap-5 grid-cols-3'>
+                    {
+                        loadedData.map(product=><Product product={product}></Product>)
+                    }
+                </div>
             </div>
         </div>
     );
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({ product }) => {
+    console.log(product);
+
+    const {product_title,price,product_image} = product
+
     return (
-        <div>
-            
+        <div className="card bg-base-100   shadow-sm">
+            <figure className="px-10 pt-10">
+                <img
+                    src={product_image}
+                    alt="Shoes"
+                    className="rounded-xl" />
+            </figure>
+            <div className="card-body px-10   text-start">
+                <h2 className="card-title">{product_title}</h2>
+                <p>Price: {price}</p>
+                <div className="card-actions">
+                    <button className="btn border-[#9538E2] font-bold text-[#9538E2] rounded-full">View details</button>
+                </div>
+            </div>
         </div>
     );
 };
